190be933cfb711abbe47cf55622ece80f3c28bc2
Simon Wagner <simon.wagner@msg.group>
2022-11-30 08:49:26 +0100

    session08: moved solutions to proper package

---
+++ session08/solution/aufgabe4/Employee.java
@@ -0,0 +1,26 @@
+package session08.solution.aufgabe4;
+
+public class Employee extends Person {
+    int employeeId;
+    String division;
+    double salary;
+
+    public Employee(int passNumber, String name, String surname, int age, float height, float weight, int employeeId, String division, double salary) {
+        super(passNumber, name, surname, age, height, weight);
+        this.employeeId = employeeId;
+        this.division = division;
+        this.salary = salary;
+    }
+
+    void sayHello() {
+        System.out.println("Hello, my name is " + this.name + " " + this.surname + " and I'm working in " + this.division + ".");
+    }
+
+    void changeDivision(String division) {
+        this.division = division;
+    }
+
+    void raiseSalary(double raise) {
+        this.salary = this.salary + raise;
+    }
+}
