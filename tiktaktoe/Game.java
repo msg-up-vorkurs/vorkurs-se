3c7429ed3e6ba5ce395cf280e88e7d060ecf3dc4
danielschaubschlaeger <daniel.schaubschlaeger@msg.group>
2022-12-05 13:32:20 +0100

    added Tic-Tac-Toe project

---
+++ tiktaktoe/Game.java
@@ -0,0 +1,42 @@
+package tiktaktoe;
+
+import tiktaktoe.utils.InputReader;
+import tiktaktoe.utils.Move;
+
+public class Game {
+
+    private InputReader inputReader = new InputReader();
+
+    private Board board = new Board();
+    private char activePlayer = 'X';
+
+    // TODO
+    private boolean gameContinues() {
+        return false;
+    }
+
+    // TODO
+    private void printEndingMessage() {}
+
+    private void printWelcomeMessage() {
+        System.out.println("Tik-Tak-Toe");
+        System.out.println("Für die Spielzüge nach der Aufforderung die Koordinaten des Feldes eingeben.");
+        System.out.println("Die Koordinaten sind mit einem Komma getrennt im Format '[Zeilenindex],[Spaltenindex]' einzugeben.");
+    }
+
+    // TODO
+    private void switchPlayer() {}
+
+    // TODO
+    public void startGame() {}
+
+    public static void main(String[] args) {
+//        Game game = new Game(new char[][]{
+//                {' ', 'X', ' '},
+//                {' ', ' ', 'O'},
+//                {' ', 'X', ' '}
+//        });
+        Game game = new Game();
+        game.startGame();
+    }
+}
